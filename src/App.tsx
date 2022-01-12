f1086b8cf2f775b780c1e139c1d8a29b8e314f57
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-01-12 13:29:49 -0700

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import { Header } from './components/Header';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 import { Rating } from './pages/Rating';
+import ReactGA from 'react-ga';
+
 
 export const App = () => {
+  const TRACKING_ID = "G-EY73HL3D48";
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.pageview(window.location.pathname);
+  
   return (
     <BrowserRouter>
       <Header />
